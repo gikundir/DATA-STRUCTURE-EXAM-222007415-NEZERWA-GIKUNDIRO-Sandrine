b3a00072f998b9332ba64ef758a4c119215df559
gikundir <nezerwagikundirosandrine@gmail.com>
2025-01-05 19:22:46 +0200

    Add files via upload

---
+++ TOPIC 3.py
@@ -0,0 +1,73 @@
+class AVLNode:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.height = 1
+
+class AVLTree:
+    def get_height(self, node):
+        if not node:
+            return 0
+        return node.height
+
+    def get_balance(self, node):
+        if not node:
+            return 0
+        return self.get_height(node.left) - self.get_height(node.right)
+
+    def rotate_right(self, y):
+        x = y.left
+        T2 = x.right
+        x.right = y
+        y.left = T2
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+        return x
+
+    def rotate_left(self, x):
+        y = x.right
+        T2 = y.left
+        y.left = x
+        x.right = T2
+        x.height = 1 + max(self.get_height(x.left), self.get_height(x.right))
+        y.height = 1 + max(self.get_height(y.left), self.get_height(y.right))
+        return y
+
+    def insert(self, root, key):
+        if not root:
+            return AVLNode(key)
+        elif key < root.key:
+            root.left = self.insert(root.left, key)
+        else:
+            root.right = self.insert(root.right, key)
+        
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+        balance = self.get_balance(root)
+
+        # Balancing the tree
+        if balance > 1 and key < root.left.key:
+            return self.rotate_right(root)
+        if balance < -1 and key > root.right.key:
+            return self.rotate_left(root)
+        if balance > 1 and key > root.left.key:
+            root.left = self.rotate_left(root.left)
+            return self.rotate_right(root)
+        if balance < -1 and key < root.right.key:
+            root.right = self.rotate_right(root.right)
+            return self.rotate_left(root)
+        return root
+
+    def inorder(self, root):
+        if not root:
+            return []
+        return self.inorder(root.left) + [root.key] + self.inorder(root.right)
+
+
+avl = AVLTree()
+root = None
+data = [10, 20, 30, 40, 50, 25]
+for key in data:
+    root = avl.insert(root, key)
+
+print("Inorder Traversal of AVL Tree:", avl.inorder(root))
